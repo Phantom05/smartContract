f4ddd2eac08a9f77e1af4f80fabb1e91b6aea105
Luke Hwang <luke@r2v.io>
2018-06-19 21:59:01 +0900

    Remove arrow functions
    - ref) https://mochajs.org/#arrow-functions

--- test/yeedtoken.js
+++ test/yeedtoken.js
@@ -1,11 +1,19 @@
 var YeedToken = artifacts.require("./YeedToken.sol")
 
-contract('YeedToken', accounts => {
-    it("should put 10 billion YeedToken in the first account", () => {
-        return YeedToken.deployed().then(instance => {
-            return instance.balanceOf.call(accounts[0]);
-        }).then(balance => {
-            assert.equal(balance.valueOf(), 10000000000, "10 billion wasn't in the first account")
+contract('YeedToken', function(accounts) {
+    describe("# Yeed 토큰 발행", function() {
+        it("should put 10 billion YeedToken in the first account", function() {
+            return YeedToken.deployed().then(function(instance) {
+                return instance.balanceOf.call(accounts[0]);
+            }).then(function(balance) {
+                assert.equal(balance.valueOf(), 10000000000, "10 billion wasn't in the first account")
+            })
+        })
+    })
+
+    describe("거래소 상장 전", function() {
+        it("신규 토큰 Deploy", function() {
+            assert.ok(false)
         })
     })
 })
