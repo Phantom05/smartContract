e5f6dd8097359caeb685300cd6817482a11b2b65
Brian Choi <brian@r2v.io>
2018-06-22 20:19:08 +0900

    add 'SafeTransferToken' contrat test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,8 @@ var YeedToken = artifacts.require("./token/YeedToken.sol");
 var SafeTokenTransfer = artifacts.require("./ico/SafeTokenTransfer.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(YeedToken, "10000000000");
+  deployer.then(async () => {
+    await deployer.deploy(YeedToken, "10000000000");
+    await deployer.deploy(SafeTokenTransfer);
+  })
 };
